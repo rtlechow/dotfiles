dec421c5c32b0fb8fe1c7ad681fa47b349d0c5b3
R.T. Lechow <rtlechow@gmail.com>
2017-11-15 14:17:14 -0500

    update provision

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,5 +1,5 @@
 function provision() {
-  ansible-galaxy install -vvv -r ~/.provision/requirements.yaml
+  ansible-galaxy install -vvv -r ~/.provision/requirements.yaml # --force to reinstall roles
   ansible-playbook -K ~/.provision/playbook.yml
 }
 
