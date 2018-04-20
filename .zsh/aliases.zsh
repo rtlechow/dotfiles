b5649db78173d4fc3b540b8ae1d6ba943f89a395
R.T. Lechow <rtlechow@gmail.com>
2018-04-20 16:25:00 -0400

    more better git-largest

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -107,6 +107,14 @@ function git-delete-squashed() {
     fi
   fi
 }
+function git_largest() {
+  git rev-list --objects --all \
+  | git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' \
+  | awk '/^blob/ {print substr($0,6)}' \
+  | sort --numeric-sort --key=2 \
+  | cut --complement --characters=13-40 \
+  | numfmt --field=2 --to=iec-i --suffix=B --padding=7 --round=nearest
+}
 
 # Postgres
 alias -g pgup='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
