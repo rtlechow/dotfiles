7542bb115681e75cb6e231969bcfef4a11c9e2a8
R.T. Lechow <rtlechow@gmail.com>
2019-11-28 11:50:46 -0500

    rg should actually use its rc file

--- .zsh/env.sh
+++ .zsh/env.sh
@@ -7,3 +7,4 @@ export JAVA_HOME='/Library/Java/Home'
 export GREP_OPTIONS='--color=auto --exclude-dir=.git'
 export GREP_COLOR='0;32'
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
