e69790afdcff24b69dd5a644a1b4ccbef8d23282
R.T. Lechow <rtlechow@gmail.com>
2023-06-01 15:02:10 -0400

    Keep old and new coreutils paths

--- .zsh/options.zsh
+++ .zsh/options.zsh
@@ -1,6 +1,7 @@
 typeset -U path manpath gem_home gem_path
 path=(~/.bin /usr/local/bin /usr/local/sbin $path)
 path=(/opt/homebrew/opt/coreutils/libexec/gnubin $path)
+path=(/usr/local/opt/coreutils/libexec/gnubin $path)
 path=(/usr/local/opt/mysql-client/bin $path)
 path=($GOPATH/bin $path)
 manpath=(/usr/local/opt/coreutils/libexec/gnuman/ $manpath)
