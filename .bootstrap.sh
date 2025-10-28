2ede74ed6d5e4858343ba3e67993ba7ab07799d6
R.T. Lechow <rtlechow@gmail.com>
2025-10-28 12:33:19 -0400

    replace ansible with mise and Brewfile

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -5,16 +5,12 @@
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/rtl/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# dotfiles need hub, provisioning needs ansible
-brew install hub ansible
-
 # Install dotfiles
 git init .
 git remote add origin https://github.com/rtlechow/dotfiles.git
 git pull origin master
 
+brew install
+
 # Vim plugins
 git submodule update --init --recursive
-
-# Ansible asks for sudoer password at the start, and before installing vagrant (just after the-unarchiver)
-provision
